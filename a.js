004402d3be3a701c7babfe281caf7c125dd7813f
Hamza Dahmani <madebyhamzadahmani@gmail.com>
2020-12-04 22:57:00 +0100

    adding time and quotes

--- a.js
+++ a.js
@@ -3,8 +3,14 @@ const data = fetch('https://type.fit/api/quotes')
 .then(res => llog(res));
 
 const p = document.querySelector('#qt')
+const Time = document.querySelector('.time')
 function llog(res) {
     p.innerHTML = res[Math.floor(Math.random() * res.length)].text
 }
 
+let Dat = new Date();
+let hours = Dat.getHours()
+let minutes = Dat.getMinutes()
+let c = Dat.()
+Time.innerHTML = `${hours} : ${minutes} in ${c}`
 
