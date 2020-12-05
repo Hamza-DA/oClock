55a47b52857bc983f4eadd6eda1876255140e13a
Hamza Dahmani <madebyhamzadahmani@gmail.com>
2020-12-05 21:38:29 +0100

    fiing time, quotes text and author, adding css media querr

--- a.js
+++ a.js
@@ -1,16 +1,34 @@
+const p = document.querySelector('#qt')
+const auth = document.querySelector('.auth')
+const Time = document.querySelector('.time')
+const greeting = document.querySelector('.greet')
+const backGround = document.querySelector('.backGround')
+const wea =document.querySelector('.wea')
+
 const data = fetch('https://type.fit/api/quotes')
 .then(res => res.json())
 .then(res => llog(res));
 
-const p = document.querySelector('#qt')
-const Time = document.querySelector('.time')
 function llog(res) {
-    p.innerHTML = res[Math.floor(Math.random() * res.length)].text
+    // p.innerHTML = `"${res[Math.floor(Math.random() * res.length)].text}"`
+    let quote = res[Math.floor(Math.random() * res.length)]
+    let {text, author} = quote;
+    p.innerHTML = `"${quote.text}"`;
+    auth.innerHTML = `- ${quote.author}`
 }
 
 let Dat = new Date();
 let hours = Dat.getHours()
 let minutes = Dat.getMinutes()
-let c = Dat.()
-Time.innerHTML = `${hours} : ${minutes} in ${c}`
+if (hours >= 12) {
+    greeting.innerHTML = `Good evening ! it's currently`
+    backGround.src = './night.jpg'
+    wea.classList.add('fa-moon')
+}else if(hours <= 12){
+    greeting.innerHTML = `Good morning ! it's currently`
+    backGround.src = './day.jpg'
+    wea.classList.add('fa-sun')
+}
+
+Time.innerHTML = `${hours} : ${minutes}`
 
